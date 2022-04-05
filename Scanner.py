85aa4b26afb6c9ad8079950874284e1e45a3010e
toslosh <93013365+toslosh@users.noreply.github.com>
2022-04-05 11:04:09 +0300

    Update and rename README.md to Scanner.py

---
+++ Scanner.py
@@ -0,0 +1,109 @@
+import os
+import time
+import random
+randomtext1 = "Done! The login and password file are saved in site.txt"
+randomtext2 = "Mistake! It is necessary to try again"
+randomtext = random.randomint(randomtext1, randomtext2)
+password = "18901, 70719, 16493, 58390,"
+login = "Myha, Yanjot, Elvin, Moryak"
+os.system("clear")
+sleep(0.5)
+loginn = input("Enter your login : ")
+sleep(0.7)
+if log in login:
+  sleep(0.4)
+  passwordd = input("Enter your password : ")
+  if passwordd in password:
+    sleep(0.4)
+    os.system("clear")
+    time.sleep(0.5)
+    choice = "Y":
+    
+    while choice.lower() == "Y":
+      sleep(0.5)
+      elementes = input("Additional files need to be uploaded! Press Y to install ie or another key to terminate the program.: ")
+      sleep(0.5)
+      print("Please wait...")
+      os.system("apt update")
+      os.system("apt upgrade")
+      os.system("apt install python3")
+      sleep(2)
+      os.system("clear")
+      time.sleep(0.3)
+      def out_red(text):
+        print("\033[31m {}" .format(text))
+      out_red("|\/| √ |-| /-\")
+      time.sleep(0.5)
+      out_red("Loading...")
+      sleep(0.7)
+      out_red("lOading...")
+      sleep(0.7)
+      out_red("loAding...")
+      sleep(0.7)
+      out_red("loaDing...")
+      sleep(0.7)
+      out_red("loadIng...")
+      sleep(0.7)
+      out_red("loadiNg...")
+      sleep(0.7)
+      out_red("loadinG...")
+      sleep(0.7)
+      out_red("Loading.")
+      time.sleep(0.4)
+      out_red("lOading..")
+      time.sleep(0.4)
+      out_red("Loading...")
+      time.sleep(0.1)
+      os.system("clear")
+      time.sleep(1)
+      out_red("MY - Tools")
+      def out_yellow(text):
+        print("\033[33m {}" .format(text))
+      out_yellow("1 - SQLMap Scanner")
+      out_yellow("2 - Web Hacking")
+      out_yellow("3 - SocialSpy")
+      out_yellow("4 - MY Console")
+      choicese = input("Select an item from the menu : ")
+      if choicese == "1":
+        time.sleep(0.2)
+        os.system("clear")
+        time.sleep(0.4)
+        print("Please, wait...")
+        import requests
+        os.system("apt update")
+        os.system("pkg update")
+        sleep(5.8)
+        sqlscan = input("Done! Enter the site to scan : ")
+        time.sleep(0.2)
+        print("Please, wait...")
+        time.sleep(38)
+        print(randomtext)
+      if choicese == "2":
+        time.sleep(0.2)
+        os.system("clear")
+        sleep(4)
+        print("Error! You dont have Root")
+      if choicese == "3":
+        time.sleep(2)
+        os.system("clear")
+        sleep(4.3)
+        print("Error! A network adapter is required")
+      if choicese == "4":
+        time.sleep(1)
+        os.system("clear")
+        print("Необходимо загрузить инструмент MY - Console ")
+        print("You need to download the tool MY - Console")
+      else:
+        time.sleep(1)
+        print("Wrong Number")
+    sleep(0.5)
+    print("Termination of the progamm...")
+    sleep(2)
+    quit()
+        quit()
+  else:
+    sleep(0.4)
+    print("Invalid password")
+else:
+  sleep(0.4)
+  print("Invalid username!")
